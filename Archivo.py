0ffbe0407212e7251f34f4cef9e091517233dd2e
Brandon P <brad.cr7@gmail.com>
2023-08-10 21:24:14 -0600

    Se cambia el archivo a un programa que puede imprimir un tipo de piramide.

--- Archivo.py
+++ Archivo.py
@@ -1,2 +1,9 @@
-print("Soy un humano")
-print("Hola soy Brandon")
+entrada_1 = input("Ingrese el tipo de pirámide: ")
+entrada_2 = input("Ingrese los niveles de la pirámide: ")
+entrada_3 = input("Ingrese el caracter que desea que tenga el tipo de pirámide: ")
+
+if entrada_1 == "Piramide Superior":
+    for fila in range(int(entrada_2)):
+        for columna in range(fila + 1):
+            print(entrada_3, end="")
+        print()
